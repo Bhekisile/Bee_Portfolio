1e35eec816fc8dfc3dcaf674450496623248b5ad
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-04-10 17:35:51 +0200

    Edit ESLint error

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const menu = document.getElementById("ham-menu");
-const ham = document.getElementById("menu");
+const menu = document.getElementById('ham-menu');
+const ham = document.getElementById('menu');
 function clickMenu() {
-  menu.style.display = "block";
-  ham.style.display = "none";
+  menu.style.display = 'block';
+  ham.style.display = 'none';
 }
+
 function clickX() {
-  menu.style.display = "none";
-  ham.style.display = "block";
+  menu.style.display = 'none';
+  ham.style.display = 'block';
 }
+
+let getHam = document.querySelector('.getHam');
+getHam.addEventListener('click', clickMenu);
+
+let x = document.querySelector('#x');
+x.addEventListener('click', clickX);
