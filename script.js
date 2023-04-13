298e88b73939a358b790973c1bf5846b94928a48
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-04-13 10:18:36 +0200

    Add mobile popup window

--- script.js
+++ script.js
@@ -17,4 +17,77 @@ const x = document.querySelector('#x');
 x.addEventListener('click', clickX);
 
 
-const mainCardArray = ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'];
+let he1 = document.getElementById("popup-content");
+let heading = "Keeping track of hundreds of components";
+let h1 = document.createElement("h1");
+h1.innerText = heading;
+he1.appendChild(h1);
+
+
+let para = document.getElementById("js-p");
+let lorem =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.";
+let p = document.createElement("p");
+p.innerText = lorem;
+para.appendChild(p);
+
+let para2 = document.getElementById("js-p2");
+let lorem2 =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.";
+let p2 = document.createElement("p");
+p2.innerText = lorem2;
+para2.appendChild(p2);
+
+let pops = document.getElementById("pop-outer");
+
+function clickPopX() {
+  pops.style.display = "none";
+}
+
+function pop() {
+  pops.style.display = "block";
+}
+
+const dataCards = [
+  {
+    id: 0,
+    image: './ImageP1/Snapshoot Portfolio1.png',
+    techStack: {
+      tech1: 'Codekit',
+      tech2: 'GitHub',
+      tech3: 'JavaScript',
+      tech4: 'Bootstrap',
+      tech5: 'Terminal',
+      tech6: 'Codepen',
+    },
+  },
+  {
+    id: 1,
+    image: './ImageP1/Snapshoot Portfolio.png',
+    techStack: {
+      tech1: 'Ruby on Rails',
+      tech2: 'CSS',
+      tech3: 'JavaScript',
+  },
+},
+]
+
+    const popup = document.getElementById("popup");
+    let img = document.createElement("img")
+    img.src = dataCards[1].image;
+    popup.appendChild(img);
+
+    var ul = document.getElementById("badges");
+    var li = document.createElement("li");
+    li.innerHTML = `
+      <li class="badge">${dataCards[1].techStack.tech1}</li>
+      <li class="badge">${dataCards[1].techStack.tech2}</li>
+      <li class="badge">${dataCards[1].techStack.tech3}</li>
+      `
+    ul.append(li);
+    
+    
+
+
+
+
