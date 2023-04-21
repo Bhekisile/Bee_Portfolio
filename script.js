36b5e7ef0a2c7b9493dd394ece62958c30dadcf2
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-04-21 15:31:45 +0200

    Edit style

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const dataCards = [
     image: './ImageP1/Snapshoot-Portfolio1.png',
     heading: 'Keeping track of hundreds of components',
     techStack: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap'],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
     linkLiveVersion: 'https://www.google.com',
     linkSource: 'https://github.com/bhekisile/',
   },
@@ -37,7 +37,7 @@ const dataCards = [
     image: './ImageP1/Snapshoot-Portfolio1.png',
     heading: 'Keeping track of hundreds of components',
     techStack: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap'],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
     linkLiveVersion: 'https://www.google.com',
     linkSource: 'https://github.com/bhekisile/',
   },
@@ -55,7 +55,7 @@ const dataCards = [
     image: './ImageP1/Snapshoot-Portfolio1.png',
     heading: 'Keeping track of hundreds of components',
     techStack: ['Codekit', 'GitHub', 'JavaScript', 'Bootstrap'],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
     linkLiveVersion: 'https://www.google.com',
     linkSource: 'https://github.com/bhekisile/',
   },
@@ -73,13 +73,11 @@ const dataCards = [
 const popupBtns = document.querySelectorAll('.pwin');
 const popupBtnsArray = Array.from(popupBtns);
 const popup = document.getElementById('popup');
-const overlay = document.getElementById('overlay');
 
-dataCards.forEach((card) => {
+dataCards.forEach((card, i) => {
   const temp = document.createElement('div');
   temp.innerHTML = `
-  
-  <div class='popupCont'>
+  <div class='popupCont hidden'>
   <button class='btn-close'>&times;</button>
   <div class='img1'><img class='pop-img' src=${card.image}></div>
   <h3>${card.heading}</h3>
@@ -88,13 +86,13 @@ dataCards.forEach((card) => {
   </ul>
   <div class='text'>${card.description}</div>
   <div class="card-button">
-        <button class='btn-1>
+        <button class='btn-1'>
           <a href=${card.linkLiveVersion} class="btn-live">
             <span class='live-text'>See live</span>
             <img src="./ImageP1/live-icon.png" alt="see live icon">
           </a>
         </button>
-        <button class='btn-2>
+        <button class='btn-2'>
           <a href=${card.linkSource} class="btn-source">
             <span class='live-text'>See source</span>
             <img src="./ImageP1/github-w.png" alt="see github icon">
@@ -103,31 +101,36 @@ dataCards.forEach((card) => {
   </div>
   </div>
   <div class='active' id='overlay'></div>
-  
   `;
   popup.append(temp);
 });
+const overlay = document.getElementById('overlay');
+console.log('overlay', overlay);
 const clicked = () => {
   popup.classList.remove('hidden');
   popup.classList.add('visible');
-  // overlay.classList.remove('hidden');
-  overlay.classList.add('active');
 };
 popupBtnsArray.forEach((btn) => {
   btn.addEventListener('click', clicked);
 });
 const popupConts = document.querySelectorAll('.popupCont');
 const closeBtns = document.querySelectorAll('.btn-close');
-for (let i = 0; i < popupConts.length; i += 1) {
-  for (let j = 0; j < popupBtns.length; j += 1) {
+for (let i = 0; i < popupConts.length; i+=1) {
+  for (let j = 0; j < popupBtns.length; j+=1) {
     popupBtns[i].onclick = function openPopup() {
+      overlay.style.visibility = 'visible';
       popupConts[i].style.visibility = 'visible';
+      popupConts[i].classList.remove('hidden')
+      popupConts[i].classList.add('visible')
     };
     closeBtns[i].onclick = function closePopup() {
       popupConts[i].style.visibility = 'hidden';
+      popupConts[i].classList.remove('visible')
+      popupConts[i].classList.add('hidden')
+      overlay.style.visibility = 'hidden';
     };
-  }
-}
+  };
+};
 
 // Form validation
 function formValidation() {
